25b5f7ba6531fa8fa87122a8c0cdebc67d8dcdb2
unknown <povilas.pofka54@gmail.com>
2021-05-20 09:38:56 +0300

    create server and install file

---
+++ server/router/router.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+// const valid = require('../middleware/validator')
+// const create = require('../controllers/main')
+
+
+module.exports = router
